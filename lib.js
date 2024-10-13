7c9dfbe90ab4ac73734fd3a5d10d6fd81a6c12f4
Nathoggles <mawricus@gmail.com>
2024-10-13 21:59:54 +0200

    make a functional shelf effect

--- lib.js
+++ lib.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, date, location){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.date = date;
+    this.location = location;
 }
 
 function addBookToLibrary(book){
